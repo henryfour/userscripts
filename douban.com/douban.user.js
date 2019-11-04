c4028ce5580647767c5013d2041465b52b6a964f
HenryFour <me@henryfour.com>
2019-11-04 10:19:58 +0800

    add comments for importing to openuserjs

--- douban.com/douban.user.js
+++ douban.com/douban.user.js
@@ -2,14 +2,18 @@
 // @name         豆瓣私有设置
 // @version      0.0.1
 // @description  豆瓣操作自动设置仅自己可见
-// @match        *://*.douban.com/*
-// @run-at       document-body
-// @icon         https://img3.doubanio.com/favicon.ico
+//
 // @namespace    userscripts.henryfour.com
 // @homepageURL  https://github.com/henryfour/userscripts
 // @supportURL   https://github.com/henryfour/userscripts/issues
 // @author       HenryFour
-// @copyright    2018, HenryFour
+// @copyright    2018+, HenryFour
+// @license      MIT
+//
+// @match        *://*.douban.com/*
+// @run-at       document-body
+// @icon         https://img3.doubanio.com/favicon.ico
+// @require      https://cdn.bootcss.com/jquery/2.2.4/jquery.min.js
 // ==/UserScript==
 
 $('body').click(function(e) {
