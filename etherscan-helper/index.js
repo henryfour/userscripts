1e7b8a4e0670c85db09ab8d4034dc9a90ecce305
HenryFour <me@henryfour.com>
2020-04-21 23:20:10 +0800

    auto commit by script

---
+++ etherscan-helper/index.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         etherscan-helper
+// @version      0.0.1
+// @description  Gadget for etherscan.io
+// @namespace    userscripts.henryfour.com
+// @homepageURL  https://github.com/henryfour/userscripts
+// @supportURL   https://github.com/henryfour/userscripts/issues
+// @author       HenryFour
+// @copyright    2020+, HenryFour
+// @license	     MIT
+//
+// @run-at       document-end
+// @grant        GM_addStyle
+// @match        https://etherscan.io/*
+// @icon         https://etherscan.io/images/favicon2.ico
+// @require      https://cdn.bootcss.com/jquery/2.2.4/jquery.min.js
+// ==/UserScript==
+
+function init() {
+}
+
+(function() {
+  'use strict';
+
+  init();
+  // todo: 隐藏页面显示的用户名, 方便截图
+  // todo: 修改交易详情页面的事件格式 (yyyy-MM-dd)
+  // todo: 交易详情页面添加按钮, 一键复制关键信息: 交易id, 时间, 区块号, 等
+})();
